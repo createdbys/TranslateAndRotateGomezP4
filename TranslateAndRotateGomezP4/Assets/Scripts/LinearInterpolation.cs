c81811402c06da8bef1028312f8570a9dd965b35
irvinnet <irvingomez0331@gmail.com>
2023-02-14 10:29:38 -0800

    idk what change but im commiting to main

---
+++ TranslateAndRotateGomezP4/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{/*
+    // Start is called before the first frame update
+    void Start()
+    {// Returns the "from" value and returns it to the "to" value
+        float result = Mathf.Lerp(3f, 5f, 0.5f);
+    
+    // Finds a point in between two given points
+        Vector3 from = new Vector3(1f, 2f, 3f);
+        Vector3 to = new Vector3(5f, 6f, 7f);
+    
+        // Here result = (4, 5, 6)
+        Vector3 result = Vector3.Lerp(from, to, 0.75f);
+
+    }
+
+    void Update()
+    {// One of them is multiplying and the other one isnt which means the one that is multiplying is going per second rather than per frame like the other one
+        void Update()
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+        }
+
+        void Update()
+        {
+            light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f * Time.deltaTime);
+        }
+    }
+*/
+}
